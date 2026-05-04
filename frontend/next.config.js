44c904b35591fb6bd96ab0a60b2e199f8b9993dc
Aditi Singh <aditisingh0@gmail.com>
2026-05-05 01:25:41 +0530

    final fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_API_URL: "https://compiq.onrender.com",
+  },
+};
 
 module.exports = nextConfig;
