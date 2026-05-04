447ee7ecfb0ec9c732d0c8c9c12a8018d3c2db58
Aditi Singh <aditisingh0@gmail.com>
2026-05-05 01:22:26 +0530

    fix: next config error

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL: https://compiq.onrender.com;
+    NEXT_PUBLIC_API_URL: "https://compiq.onrender.com",
   },
 };
 
